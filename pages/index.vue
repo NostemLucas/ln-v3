946b6495320992fd5f7b8d20419241844d7b4be4
NostemLucas <102114238+NostemLucas@users.noreply.github.com>
2025-04-03 23:38:25 -0400

    fix: correcion de erorres

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,9 @@
     <HeroCards />
     <TreatmentCards />
     <HeroAbout />
+    <HeroCarrousel />
+    <HeroParalax />
+    <DiagnosticCards />
   </div>
 </template>
 <script lang="ts" setup>
@@ -11,4 +14,7 @@ import EnhancedVideo from "@/components/landing/EnhancedVideo.vue";
 import HeroCards from "@/components/landing/HeroCards.vue";
 import TreatmentCards from "@/components/landing/TreatmentCards.vue";
 import HeroAbout from "@/components/landing/HeroAbout.vue";
+import HeroCarrousel from "@/components/landing/HeroCarrousel.vue";
+import HeroParalax from "@/components/landing/HeroParalax.vue";
+import DiagnosticCards from "@/components/landing/DiagnosticCards.vue";
 </script>
